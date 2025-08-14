5194421809b36525efd584426d531bc984582bdd
dave zhang <davezhangus@gmail.com>
2025-08-14 02:04:14 +0000

    更新网页名字

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'ScheduleCrunch',
+  title: 'UPPCalculate',
   description: 'A special calculator for schedule utilization.',
 };
 
